73d77696292f358a3629c3a05fcbcfc96ab1ec68
LAP <2300820209@qq.com>
2018-12-29 21:56:18 +0800

    lap

--- socketBack.js
+++ socketBack.js
@@ -18,10 +18,30 @@ httpServer.listen(8080,()=>{console.log("Server is running in 8080");});
 
 let websocketServer = io.listen(httpServer);
 
-websocketServer.on('connection',function(sock){
-    // sock.on("a",function(num1,num2){
-    //     console.log(`num1:${num1},num2:${num2}`);
-    // });
-    setInterval(()=>{sock.emit("name",Math.random()+"lapXK");},2000);
+websocketServer.on('connection',function(socket){
+    socket.on("reg",(user,password)=>{
+        if(!regs.username.test(user)){
+            socket.emit("reg_ret",1,"用户名不符合规范");
+        }else if(!regs.password.test(password)){
+            socket.emit("reg_ret",1,"密码不符合规范");
+        }else{
+            de.query(`select id from user_table where username = ${user}`,(err,data)=>{
+                if(err){
+                    socket.emit('reg_ret',1,'数据库错误');
+                }else if(data.length > 0){
+                    socket.emit("reg_ret",1,"用户名已存在");
+                }else{
+                    de.query(`insert into user_table (username,password,online) values(${user},${password},0)`,(err)=>{
+                        if(err){
+                            sock.emit('reg_ret',1,'数据库错误');
+                        }else{
+                            sock.emit('reg_ret',0,'注册成功');
+                        }
+                    });
+                }
+            });
+        }
+    });
+    setInterval(()=>{socket.emit("name",Math.random()+"lapXK");},2000);
     
 });
